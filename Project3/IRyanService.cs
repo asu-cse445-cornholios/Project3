1188b70d61f78e4b967cd46d3aa01fe01f90c8d9
Ryan Foster <bubber11@gmail.com>
2012-03-10 13:01:37 -0700

    made branch

---
+++ Project3/IRyanService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Project3
+{
+    [ServiceContract]
+    public interface IRyanService
+    {
+
+        [OperationContract]
+        string GetData(string val);
+
+    }
+
+}
