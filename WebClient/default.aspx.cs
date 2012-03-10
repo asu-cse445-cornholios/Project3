ebbcf8bcb042e0c6fc42ae20d44c4592cdffb63d
Ryan Foster <bubber11@gmail.com>
2012-03-10 16:05:12 -0700

    Working tryit page with reference to services

---
+++ WebClient/default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebClient
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ServiceProxy.Service1Client client = new ServiceProxy.Service1Client();
+
+            string response = client.GetData(int.Parse(TextBox1.Text));
+            Label1.Text = response;
+        }
+    }
+}
