8ba8da4f86c8741d49126a68ea9206a27a9fa684
Rebecca Bever <rbever@asu.edu>
2012-03-10 12:46:55 -0700

    new changes

---
+++ WSDLAddress/App_Code/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+public class Service : IService
+{
+    public string[] getWsdlAddress(string url)
+	{
+        Web2String.ServiceClient webProxy = new Web2String.ServiceClient();
+        string webContent = webProxy.GetWebContent(url);
+
+        string[] sep = new string[2];
+        sep[0] = ".wsdl";
+        sep[1] = "?wsdl";
+        string[] sites = webContent.Split(sep, System.StringSplitOptions.None);
+        
+        return sites;
+	}
+}
---
+++ WSDLAddress/testWCF/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace testWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference.ServiceClient mywcf = new ServiceReference.ServiceClient();
+            
+            string[] webContent = mywcf.getWsdlAddress("http://data.serviceplatform.org/servicefinder/sf-wsdls.list.sorted");
+            foreach (string web in webContent)
+            {
+                Console.WriteLine(web);
+            }
+ 
+            mywcf.Close();
+            Console.WriteLine("Press <ENTER> to terminate the client.");
+            Console.ReadLine();
+        }
+    }
+}
