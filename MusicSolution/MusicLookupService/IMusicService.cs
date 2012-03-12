b508b79112648739140df8f4003448db1f63ac83
Ryan Foster <bubber11@gmail.com>
2012-03-11 23:01:54 -0700

    fixedd

---
+++ MusicSolution/MusicLookupService/IMusicService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+using MusicInfo;
+using MusicInfo.SearchTypes;
+
+namespace MusicLookupService
+{
+    [ServiceContract]
+    public interface IMusicService
+    {
+        [OperationContract]
+        ArtistResult[] findArtists(ArtistSearch search);
+
+        [OperationContract]
+        ReleaseGroupResult[] findReleaseGroups(ReleaseGroupSearch search);
+
+        [OperationContract]
+        ReleaseResult[] findReleases(ReleaseSearch search);
+
+        [OperationContract]
+        RecordingResult[] findRecordings(RecordingSearch search);
+
+        [OperationContract]
+        LabelResult[] findLabels(LabelSearch search);
+
+        [OperationContract]
+        WorkResult[] findWorks(WorkSearch search);
+    }
+
+
+}
---
+++ MusicSolution/MusicLookupService/MusicService.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+using MusicInfo;
+using MusicInfo.SearchTypes;
+
+namespace MusicLookupService
+{
+    public class MusicService : IMusicService
+    {
+        public ArtistResult[] findArtists(ArtistSearch search)
+        {
+            return MediaLibrary.findArtists(search);
+        }
+
+        public ReleaseGroupResult[] findReleaseGroups(ReleaseGroupSearch search)
+        {
+            return MediaLibrary.findReleaseGroups(search);
+        }
+
+        public ReleaseResult[] findReleases(ReleaseSearch search)
+        {
+            return MediaLibrary.findReleases(search);
+        }
+
+        public RecordingResult[] findRecordings(RecordingSearch search)
+        {
+            return MediaLibrary.findRecordings(search);
+        }
+
+        public LabelResult[] findLabels(LabelSearch search)
+        {
+            return MediaLibrary.findLabels(search);
+        }
+
+        public WorkResult[] findWorks(WorkSearch search)
+        {
+            return MediaLibrary.findWorks(search);
+        }
+    }
+}
