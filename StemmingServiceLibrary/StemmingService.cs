523026f85f16e860c75bf342270fe40638dcc710
Duc Phuc Tran <beaver@asu.edu>
2012-03-11 22:32:27 -0700

    Finished stemming service

--- StemmingServiceLibrary/StemmingService.cs
+++ StemmingServiceLibrary/StemmingService.cs
@@ -4,14 +4,35 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace StemmingServiceLibrary
 {
     public class StemmingService : IStemmingService
     {
+        private string regExMatch = @"\w+";
         public string Stemming(string str)
         {
-            return "I should have wrote a jive translator service";
+            if (String.IsNullOrWhiteSpace(str))
+            {
+                return String.Empty;
+            }
+            Regex regEx = new Regex(regExMatch);
+            var result = regEx.Replace(str, new MatchEvaluator(ReplaceWordWithStem));
+            return result;
+            
+        }
+
+        private string ReplaceWordWithStem(Match match)
+        {
+            if (String.IsNullOrWhiteSpace(match.Value))
+            {
+                return String.Empty;
+            }
+            else
+            {
+                return new EnglishStemmer.EnglishWord(match.Value).Stem;
+            }
         }
     }
 }
