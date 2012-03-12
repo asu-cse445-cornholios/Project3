b9230b085057f96edffc1647bdfa4c122a9af096
Ryan Foster <bubber11@gmail.com>
2012-03-11 17:46:57 -0700

    first commit of project

---
+++ MusicSolution/MusicInfo/MediaLibrary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Xml.XPath;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MusicInfo
+{
+    using SearchTypes;
+    using System.IO;
+
+
+    class MediaLibrary
+    {
+        /// <summary>
+        /// Contains base url to service
+        /// </summary>
+        private static string serviceUrl = "http://www.musicbrainz.org/ws/2"; 
+
+
+        public static ArtistResult findArtists(ArtistSearch search)
+        {
+            //
+            // Build request Url
+            //
+            string requestUrl = serviceUrl + "/artist/?query=";
+
+            if (search.Artist != "")
+            {
+                requestUrl += "artist:" + search.Artist + " ";
+            }
+
+            System.Console.WriteLine(requestUrl);
+
+            //
+            // Get and Parse result
+            //
+            XPathDocument doc = new XPathDocument(requestUrl);
+            XPathNavigator nav = doc.CreateNavigator();
+            XPathNodeIterator iter = nav.Select("/metadata/artist-list/artist");
+            
+            while (iter.MoveNext())
+            {
+                XPathNodeIterator xnode = iter.Current.Select("name");
+                System.Console.WriteLine(xnode.Current.Value);
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ MusicSolution/MusicInfo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MusicInfo
+{
+    using SearchTypes;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArtistSearch search = new ArtistSearch();
+            search.Artist = "Scorpions";
+
+            ArtistResult result = MediaLibrary.findArtists(search);
+        }
+    }
+}
