26208a1e7766aa1b4881a56570432bac9bd5b49e
Duc Phuc Tran <beaver@asu.edu>
2012-03-12 00:28:19 -0700

    Updated files for publishing

---
+++ Top10ContentWordsWebApplication/TryIt.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Top10ContentWordsWebApplication
+{
+    public partial class Top10ContentWordsServiceTryItPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
